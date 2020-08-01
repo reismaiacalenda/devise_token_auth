dc1f57f54a3c2eb48436f10678248bb084cf284c
BrusGarka <lucas.maia.verissimo@gmail.com>
2020-08-01 00:51:36 -0300

    encoding redirect uri

--- app/controllers/devise_token_auth/registrations_controller.rb
+++ app/controllers/devise_token_auth/registrations_controller.rb
@@ -27,6 +27,7 @@ module DeviseTokenAuth
         @redirect_url = @redirect_url.gsub('app', params["workspace_subdominio"]) if @redirect_url.include? "app"
         @redirect_url += "?" #/login/preparando
         @redirect_url += params.permit("workspace_convite_obrigatorio", "workspace_nome", "workspace_subdominio", "workspace_telefone").to_query
+        @redirect_url = CGI::escape @redirect_url
       end
 
       # success redirect url is required
