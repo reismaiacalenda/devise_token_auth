f02ad4243e025d435f259876a6e286e0b18179f1
BrusGarka <lucas.maia.verissimo@gmail.com>
2020-08-01 01:29:58 -0300

    criação de encode 64

--- app/controllers/devise_token_auth/registrations_controller.rb
+++ app/controllers/devise_token_auth/registrations_controller.rb
@@ -26,7 +26,9 @@ module DeviseTokenAuth
         @redirect_url = request.headers['origin']
         @redirect_url = @redirect_url.gsub('app', params["workspace_subdominio"]) if @redirect_url.include? "app"
         @redirect_url += "?" #/login/preparando
-        @redirect_url += params.permit("workspace_convite_obrigatorio", "workspace_nome", "workspace_subdominio", "workspace_telefone").to_query
+        #@redirect_url += params.permit("workspace_convite_obrigatorio", "workspace_nome", "workspace_subdominio", "workspace_telefone").to_query
+        tk = Base64.encode64(params.permit("workspace_convite_obrigatorio", "workspace_nome", "workspace_subdominio", "workspace_telefone").to_query)
+        @redirect_url += "criar_token=#{tk}
         @redirect_url = CGI::escape @redirect_url
       end
 
