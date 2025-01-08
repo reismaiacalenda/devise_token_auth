15f8665ae0b93e0b1e4e5361f7e6d7022fed3200
maiareismaia <lucas@reismaia.com>
2025-01-08 05:24:14 +0000

    remove duplicação na inicialização da blacklist de atributos inseguros e atualiza versão para 1.1.30

--- lib/devise_token_auth/blacklist.rb
+++ lib/devise_token_auth/blacklist.rb
@@ -1,8 +1,9 @@
 # don't serialize tokens
-Devise::Models::Authenticatable::UNSAFE_ATTRIBUTES_FOR_SERIALIZATION = [
-  :encrypted_password, :reset_password_token, :reset_password_sent_at,
-  :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at,
-  :current_sign_in_ip, :last_sign_in_ip, :password_salt, :confirmation_token,
-  :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :failed_attempts,
-  :unlock_token, :locked_at, :authentication_token
-]
+# Removido para evitar inicialização duplicada
+# Devise::Models::Authenticatable::UNSAFE_ATTRIBUTES_FOR_SERIALIZATION = [
+#   :encrypted_password, :reset_password_token, :reset_password_sent_at,
+#   :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at,
+#   :current_sign_in_ip, :last_sign_in_ip, :password_salt, :confirmation_token,
+#   :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :failed_attempts,
+#   :unlock_token, :locked_at, :authentication_token
+# ]
