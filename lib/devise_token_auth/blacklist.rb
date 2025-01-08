7faa477c15bc399c608edfdaf0dab8bd3277cbcb
maiareismaia <lucas@reismaia.com>
2025-01-08 05:22:27 +0000

    atualiza blacklist de atributos inseguros para serialização no Devise

--- lib/devise_token_auth/blacklist.rb
+++ lib/devise_token_auth/blacklist.rb
@@ -1,2 +1,8 @@
 # don't serialize tokens
-Devise::Models::Authenticatable::BLACKLIST_FOR_SERIALIZATION << :tokens
+Devise::Models::Authenticatable::UNSAFE_ATTRIBUTES_FOR_SERIALIZATION = [
+  :encrypted_password, :reset_password_token, :reset_password_sent_at,
+  :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at,
+  :current_sign_in_ip, :last_sign_in_ip, :password_salt, :confirmation_token,
+  :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :failed_attempts,
+  :unlock_token, :locked_at, :authentication_token
+]
